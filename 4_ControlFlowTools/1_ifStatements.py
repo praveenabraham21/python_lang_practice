945780098346198774825be93efdbf89e90edf0e
Praveen Abraham <praveenabraham2146@gmail.com>
2025-08-26 22:44:43 +0530

    first commit - 082625

---
+++ 4_ControlFlowTools/1_ifStatements.py
@@ -0,0 +1,12 @@
+def checkSign(n):
+    if n < 0:
+        print("Negative")
+    elif n == 0:
+        print("Zero")
+    elif n > 0:
+        print("Positive")
+
+
+checkSign(10)
+checkSign(-10)
+checkSign(0)
